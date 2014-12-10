d2d239ae259c3136f0ba5bbf99215c9344f448cf
mojoLyon <stephane@MacBook-Air-de-Stephane.local>
2014-12-10 12:38:39 +0100

    Add test before calling app/console in sf function

--- functions/sf.fish
+++ functions/sf.fish
@@ -1,3 +1,9 @@
 function sf --description "Symfony2 console"
-	app/console $argv
+	if test -e app/console
+		app/console $argv
+	else
+		set_color yellow
+		echo "Not in an Symfony project"
+		set_color normal
+	end
 end
